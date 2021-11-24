f1e98adee5cdecf131bc8c5d53727318443ce68f
Ohjaeyeop <39650591+Ohjaeyeop@users.noreply.github.com>
2021-11-24 13:50:23 +0900

    login & signUp modal

---
+++ src/views/app.js
@@ -0,0 +1,29 @@
+const body = document.querySelector("body");
+const loginButton = document.querySelector("#login");
+const loginModal = document.querySelector(".modal_background1");
+
+const signUpButton = document.querySelector("#signUp");
+const signUpModal = document.querySelector(".modal_background2");
+
+let isModal = false;
+
+const modalVisibility = (modal) => {
+  modal.style.display = "block";
+  isModal = true;
+};
+
+const handleClick = ({ target }) => {
+  if (target === loginButton) {
+    modalVisibility(loginModal);
+  } else if (target === signUpButton) {
+    modalVisibility(signUpModal);
+  } else {
+    if (isModal) {
+      loginModal.style.display = "none";
+      signUpModal.style.display = "none";
+      isModal = false;
+    }
+  }
+};
+
+document.addEventListener("click", handleClick);
