6f4e6dd2b7b2cb82115619d0977be6e6b1859b32
Ohjaeyeop <39650591+Ohjaeyeop@users.noreply.github.com>
2021-11-24 10:24:23 +0900

    header UI

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mysql from "mysql";
+import path from "path";
 
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "ojy5002!@",
 });
 
 con.connect(function (err) {
@@ -15,6 +16,13 @@ con.connect(function (err) {
 const PORT = 3000;
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/views"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views/home.html"))
+);
 
 app.listen(PORT, () =>
   console.log(`Server listening on port http://localhost:${PORT}🚀`)
