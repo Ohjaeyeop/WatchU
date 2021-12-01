55639e883785ab6a43747fa135f5a7fc13d17c15
SangJune0627 <yain20301@gmail.com>
2021-12-02 01:48:12 +0900

    login,signup

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,16 +1,20 @@
 import { render } from "ejs";
-import { searchByTitle } from "../db.js";
+import { searchMovieByTitle, searchAllMovie, searchUserById, signUpDb, logIn, searchMovieById } from "../db.js";
 
 export const home = (req, res) => {
   // 모든 영화 리스트 db에서 가져오기
-
-  return res.render("home");
+  searchAllMovie(function (err, result) {
+    if (err) throw err;
+    else {
+      return res.render("home", { movies: result });
+    }
+  });
 };
 
 export const search = (req, res) => {
   const { title } = req.query;
 
-  searchByTitle(title, function (err, result) {
+  searchMovieByTitle(title, function (err, result) {
     if (err) throw err;
     else {
       return res.render("search", { movies: result });
@@ -24,6 +28,18 @@ export const search = (req, res) => {
 export const signUp = (req, res) => {
   const { name, id, password } = req.body;
   console.log(req.body);
+
+  searchUserById(id, function (err, result) {
+    if (err) throw err;
+    else if (result.length == 0) {    // 없는 id면 sign up
+      signUpDb(id, name, password, function (err, result) {
+        if (err) throw err;
+        else return res.render("signUp", { message: "Sign Up" });
+      })
+    }
+    else return res.render("signUp", { message: "Already exist" }); // 이미 존재하는 id
+  });
+
   // DB에 이미 존재하는 id인지 확인
   // 존재하지 않는다면 user 생성 (user 테이블에 insert 해주기)
 };
@@ -31,6 +47,11 @@ export const signUp = (req, res) => {
 export const login = (req, res) => {
   const { id, password } = req.body;
   console.log(req.body);
+
+  logIn(id, password, function (err, result) {
+    if (err) throw err;
+    else return res.render("login", { user: result });
+  });
   // DB에 존재하는 id인지 확인
   // 존재한다면 id와 password가 일치하는지 확인
   // 일치한다면 해당 user의 정보 리턴
@@ -41,6 +62,9 @@ export const detail = (req, res) => {
   console.log(id);
 
   // id로 영화검색 후 해당 영화정보 리턴
+  searchMovieById(id, function (err, result) {
+    if (err) throw err;
+    else return res.render("detail", { movie: result });
+  });
 
-  return res.render("detail");
 };
--- src/db.js
+++ src/db.js
@@ -5,84 +5,167 @@ import e from "express";
 var con;
 
 export function init() {
-  con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "ojy5002!@",
-  });
+    con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "ojy5002!@",
+    });
 
-  con.connect(function (err) {
-    if (err) throw err;
-    console.log("DB is connected ✅");
-  });
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("DB is connected ✅");
+    });
 
-  con.query("DROP DATABASE IF EXISTS testdb", function (err, result) {
-    if (err) throw err;
-    console.log("DB is deleted ✅");
-  });
+    con.query("DROP DATABASE IF EXISTS testdb", function (err, result) {
+        if (err) throw err;
+        console.log("DB is deleted ✅");
+    });
 
-  con.query("CREATE DATABASE testdb", function (err, result) {
-    if (err) throw err;
-    console.log("DB is created ✅");
-  });
+    con.query("CREATE DATABASE testdb", function (err, result) {
+        if (err) throw err;
+        console.log("DB is created ✅");
+    });
 
-  con.changeUser({ database: "testdb" }, function (err) {
-    if (err) throw err;
-    console.log("DB is reconnected ✅");
-  });
+    con.changeUser({ database: "testdb" }, function (err) {
+        if (err) throw err;
+        console.log("DB is reconnected ✅");
+    });
 
-  createTables();
-  insertData();
+    createTables();
+    insertData();
 
-  searchByTitle("The", function (err, result) {
-    if (err) throw err;
-    else console.log(result);
-  });
+
+    // searchMovieByTitle("The", function (err, result) {
+    //     if (err) throw err;
+    //     else console.log(result);
+    // });
+
+    // searchAllMovie(function (err, result) {
+    //     if (err) throw err;
+    //     else console.log(result);
+    // });
+
+
+    // var id = "yain20301";
+    // var name_g = "SangJune";
+    // var password = "yain1015";
+
+
+    // searchUserById(id, function (err, result) {
+    //     if (err) throw err;
+    //     else if (result.length == 0) {
+    //         signUpDb(id, name_g, password, function (err, result) {
+    //             if (err) throw err;
+    //             else console.log("Sign Up!");
+    //         })
+    //     }
+    //     else console.log("Already exist");
+    // });
+
+    // logIn(id, password, function (err, result) {
+    //     if (err) throw err;
+    //     else console.log(result);
+    // });
+
+    // searchMovieById(1, function (err, result) {
+    //     if (err) throw err;
+    //     else console.log(result);
+    // });
 }
 
-export function searchByTitle(title, callback) {
-  var sql = `SELECT * FROM movie WHERE title LIKE \"%${title}%\"`;
-  con.query(sql, function (err, result) {
-    if (err) callback(err, null);
-    else callback(null, result);
-  });
+export function logIn(id, password, callback) {
+    var sql = `SELECT * FROM watchu_user WHERE id=\"${id}\" and password=\"${password}\"`
+
+    con.query(sql, function (err, result) {
+        if (err) callback(err, null);
+        else callback(null, result);
+    });
+}
+
+
+export function signUpDb(id, name, password, callback) {
+    var sql = `INSERT INTO watchu_user (id, name, password) VALUES (\"${id}\",\"${name}\",\"${password}\")`
+
+    con.query(sql, function (err, result) {
+        if (err) callback(err, null);
+        else callback(null, result);
+    });
+}
+
+export function searchUserById(id, callback) {
+    var sql = `SELECT * FROM watchu_user WHERE id=\"${id}\"`
+
+    con.query(sql, function (err, result) {
+        if (err) callback(err, null);
+        else callback(null, result);
+    });
+}
+
+export function searchMovieById(id, callback) {
+    var sql = `SELECT * FROM movie WHERE id=${id}`;
+    con.query(sql, function (err, result) {
+        if (err) callback(err, null);
+        else callback(null, result);
+    });
+}
+
+export function searchMovieByTitle(title, callback) {
+    var sql = `SELECT * FROM movie WHERE title LIKE \"%${title}%\"`;
+    con.query(sql, function (err, result) {
+        if (err) callback(err, null);
+        else callback(null, result);
+    });
+}
+
+export function searchAllMovie(callback) {
+    var sql = "SELECT * FROM movie";
+    con.query(sql, function (err, result) {
+        if (err) callback(err, null);
+        else callback(null, result);
+    });
 }
 
 function createTables() {
-  var create_user_sql =
-    "CREATE TABLE watchu_user (id INT PRIMARY KEY, name VARCHAR(255), password VARCHAR(255),  gender VARCHAR(255), age INT)";
-  con.query(create_user_sql, function (err, result) {
-    if (err) throw err;
-    console.log("User Table created");
-  });
-  var create_movie_sql =
-    "CREATE TABLE movie (id INT PRIMARY KEY, title VARCHAR(255), year VARCHAR(255) ,genre VARCHAR(255), rating FLOAT, poster VARCHAR(255))";
-  con.query(create_movie_sql, function (err, result) {
-    if (err) throw err;
-    console.log("Movie Table created");
-  });
+    var create_user_sql =
+        "CREATE TABLE watchu_user (id VARCHAR(255) PRIMARY KEY, name VARCHAR(255), password VARCHAR(255))";
+    con.query(create_user_sql, function (err, result) {
+        if (err) throw err;
+        console.log("User Table created");
+    });
+    var create_movie_sql =
+        "CREATE TABLE movie (id INT PRIMARY KEY, title VARCHAR(255), year VARCHAR(255) ,genre VARCHAR(255), rating FLOAT, poster VARCHAR(255))";
+    con.query(create_movie_sql, function (err, result) {
+        if (err) throw err;
+        console.log("Movie Table created");
+    });
 }
 
 function insertData() {
-  var movie_data = fs
-    .readFileSync("data/movie.dat", "utf8")
-    .toString()
-    .split("\n");
-  var array = [];
-  for (var i of movie_data) {
-    var movie = i.split("::");
-    array.push([
-      parseInt(movie[0]),
-      movie[1],
-      movie[2],
-      movie[3],
-      parseFloat(movie[4]),
-      movie[5],
-    ]);
-  }
-  var sql = "INSERT INTO movie (id,title,year,genre,rating,poster) VALUES ?";
-  con.query(sql, [array], function (err) {
-    if (err) throw err;
-    console.log("Movie inserted");
-  });
+    var movie_data = fs
+        .readFileSync("data/movie.dat", "utf8")
+        .toString()
+        .split("\n");
+    var array = [];
+    for (var i of movie_data) {
+        var movie = i.split("::");
+        array.push([
+            parseInt(movie[0]),
+            movie[1],
+            movie[2],
+            movie[3],
+            parseFloat(movie[4]),
+            movie[5],
+        ]);
+    }
+    var sql = "INSERT INTO movie (id,title,year,genre,rating,poster) VALUES ?";
+    con.query(sql, [array], function (err) {
+        if (err) throw err;
+        console.log("Movie inserted");
+    });
+
+    var sql = "INSERT INTO watchu_user (id, name, password) VALUES (\"yain20301\",\"SangJune\",\"yain1015\")";
+    con.query(sql, function (err) {
+        if (err) throw err;
+        console.log("User inserted");
+    });
 }
