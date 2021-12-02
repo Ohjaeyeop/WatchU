a4ba584f5634f3fe87859da1a6c2325160d94b48
Ohjaeyeop <39650591+Ohjaeyeop@users.noreply.github.com>
2021-12-02 21:12:26 +0900

    about user

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,12 +1,4 @@
-import { render } from "ejs";
-import {
-  searchMovieByTitle,
-  searchAllMovie,
-  searchUserById,
-  signUpDb,
-  logIn,
-  searchMovieById,
-} from "../db.js";
+import { searchMovieByTitle, searchAllMovie, searchMovieById } from "../db.js";
 
 export const home = (req, res) => {
   // 모든 영화 리스트 db에서 가져오기
@@ -32,38 +24,6 @@ export const search = (req, res) => {
   // 해당 영화 정보 리턴
 };
 
-export const signUp = (req, res) => {
-  const { name, id, password } = req.body;
-  console.log(req.body);
-
-  searchUserById(id, function (err, result) {
-    if (err) throw err;
-    else if (result.length == 0) {
-      // 없는 id면 sign up
-      signUpDb(id, name, password, function (err, result) {
-        if (err) throw err;
-        else return res.render("signUp", { message: "Sign Up" });
-      });
-    } else return res.render("signUp", { message: "Already exist" }); // 이미 존재하는 id
-  });
-
-  // DB에 이미 존재하는 id인지 확인
-  // 존재하지 않는다면 user 생성 (user 테이블에 insert 해주기)
-};
-
-export const login = (req, res) => {
-  const { id, password } = req.body;
-  console.log(req.body);
-
-  logIn(id, password, function (err, result) {
-    if (err) throw err;
-    else return res.render("login", { user: result });
-  });
-  // DB에 존재하는 id인지 확인
-  // 존재한다면 id와 password가 일치하는지 확인
-  // 일치한다면 해당 user의 정보 리턴
-};
-
 export const detail = (req, res) => {
   const { id } = req.params;
   console.log(id);
@@ -71,6 +31,9 @@ export const detail = (req, res) => {
   // id로 영화검색 후 해당 영화정보 리턴
   searchMovieById(id, function (err, result) {
     if (err) throw err;
-    else return res.render("detail", { movie: result });
+    else {
+      console.log(result);
+      return res.render("detail", { movie: result });
+    }
   });
 };
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,20 +1,37 @@
+import { searchUserById, signUpDb, logIn } from "../db.js";
+
 export const signUp = (req, res) => {
   const { name, id, password } = req.body;
   console.log(req.body);
-  // DB에 이미 존재하는 id인지 확인
-  // 존재하지 않는다면 user 생성 (user 테이블에 insert 해주기)
-  req.session.loggedIn = true;
-  return res.redirect("/");
+
+  searchUserById(id, function (err, result) {
+    if (err) throw err;
+    else if (result.length == 0) {
+      // 없는 id면 sign up
+      signUpDb(id, name, password, function (err, result) {
+        if (err) throw err;
+        else return res.redirect("/");
+      });
+    } else throw "already exist";
+  });
 };
 
 export const login = (req, res) => {
   const { id, password } = req.body;
   console.log(req.body);
+
+  logIn(id, password, function (err, result) {
+    if (result.length === 0) return res.redirect("/");
+    else {
+      req.session.loggedIn = true;
+      req.session.user = result;
+      console.log(result);
+      return res.redirect("/");
+    }
+  });
   // DB에 존재하는 id인지 확인
   // 존재한다면 id와 password가 일치하는지 확인
   // 일치한다면 해당 user의 정보 리턴
-  req.session.loggedIn = true;
-  return res.redirect("/");
 };
 
 export const logout = (req, res) => {
